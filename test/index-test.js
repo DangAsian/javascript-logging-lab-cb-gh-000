18b80232507e251f245e2d67e953d0137df91ef1
Daniel Ang <dan.justin.ang@gmail.com>
2018-11-22 00:49:58 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -7,12 +7,12 @@ const path = require('path')
 describe('index', () => {
   const html = '<div></div>'
   const src = path.resolve(__dirname, '..', 'index.js')
+
   it('calls console.error()', done => {
     const spy = expect.spyOn(console, 'error').andCallThrough()
-
+    console.error("Halp")
     jsdom.env(html, [src], {
       virtualConsole: jsdom.createVirtualConsole().sendTo(console)
-      console.error("Halp")
     }, (err, window) => {
       expect(spy).toHaveBeenCalled('expected console.error to have been called')
       console.error.restore()
